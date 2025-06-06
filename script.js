092d1b173ecbd4852ac28909a806c477b65cee83
xmgrnd <171977173+xmgrnd@users.noreply.github.com>
2025-06-06 17:32:42 +0200

    Add changing canvas pixel size by canvas size

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 const grid = document.querySelector(".canvas-container");
 let selectedColor = "black";
 
-function gridCrafter(size) {
+grid.addEventListener('contextmenu', (e) => {
+    e.preventDefault();});
+
+let isRightPressed = false;
+grid.addEventListener("mousedown", (e) => {
+        e.preventDefault();
+        isRightPressed = true;});
+
+grid.addEventListener("mouseup", (e) => {
+        e.preventDefault();
+        isRightPressed = false;});
+
+
+//creating grid, 2 loops: first for columns second for rows
+//Each box created, new event listener created (unoptimized?)
+function gridCrafter(size, pxSize) {
+    document.querySelector(".left-container p")
+        .textContent = `Canvas ${size}x${size}`;
     for (let i = 0; i < size; i++) {
         const newBoxContainer = document.createElement("div");
         newBoxContainer.classList.add(`grid-column`);
@@ -9,10 +26,12 @@ function gridCrafter(size) {
         console.log(`Added ${i+1} grid column.`)
         for (let j = 0; j < size; j++) {
             const newBox = document.createElement("div");
+            newBox.style.width = pxSize;
+            newBox.style.height = pxSize;
             newBox.classList.add("grid-child");
-                    newBox.addEventListener("mouseover", () =>{ 
-                    newBox.style["background-color"] = selectedColor;
-                    });
+            newBox.addEventListener("mouseover", (e) =>{ 
+                    if (isRightPressed) newBox.style["background-color"] = selectedColor;
+                });
             newBoxContainer.appendChild(newBox);
             console.log(`Added ${j+1} grid box.`)
         }
@@ -20,6 +39,8 @@ function gridCrafter(size) {
     console.log(`Gridbox creation finished`);
 }
 
+
+//Removing grid by iterating through columns
 function gridDestroyer() {
     console.log("[DESTORYER] called");
     const nodelist = document.querySelectorAll(".grid-column");
@@ -29,8 +50,24 @@ function gridDestroyer() {
     console.log(`[DESTORYER] ended`);
 }
 
+// well, needs optimization
+function setPixelSize(size) {
+    let s = "";
+    if (size > 90) s = "5px";
+    else if (size > 80) s = "6px";
+    else if (size > 70) s = "7px";
+    else if (size > 60) s = "8px";
+    else if (size > 50) s = "9px";
+    else if (size > 40) s = "10px";
+    else if (size > 30) s = "11px";
+    else if (size > 40) s = "12px";
+    else if (size > 30) s = "13px";
+    else if (size > 20) s = "14px";
+    else if (size > 10) s = "15px";
+    else s = "16px";
+    return s;
+}
 
-
-
-gridCrafter(100);
+let value = 50;
+gridCrafter(value, setPixelSize(value));
 
